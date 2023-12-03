e5121a59b8359c46bb3d0c172a993116dd2189da
mj <mj@home.lab>
2023-12-03 12:31:56 +0200

    add extensions installation/backup

--- backup.sh
+++ backup.sh
@@ -15,6 +15,8 @@ pip list | awk '{print $1}' | sed '1d;2d' > backup/python_list.txt
 ls -1 ~/.local/share/gnome-shell/extensions/ > backup/gnome_extension_list.txt
 # User crontab
 crontab -l > backup/user_crontab.txt
+# Gnome extensions configuration
+dconf load /org/gnome/shell/extensions/ > backup/extensions_settings.txt
 
 # Backup dotfiles
 DOTFILES=".zshrc .zshenv .vimrc .p10k.zsh .p10k-8color.zsh .alias.zsh"
