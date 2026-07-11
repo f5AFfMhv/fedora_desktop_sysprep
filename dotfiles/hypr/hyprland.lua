5ffb6835dce5744e65b7cf4db27671c6734a3bb9
Martynas <mjankunas@gmail.com>
2026-07-11 16:09:12 +0300

    backup 2026-07-11

---
+++ dotfiles/hypr/hyprland.lua
@@ -0,0 +1,14 @@
+hl.on("hyprland.start", function()
+  hl.exec_cmd("noctalia")
+end)
+
+hl.layer_rule({
+  name = "noctalia",
+  match = {
+    namespace = "^noctalia-(bar-.+|notification|dock|panel|attached-panel|osd)$",
+  },
+  no_anim = true,
+  ignore_alpha = 0.5,
+  blur = true,
+  blur_popups = true,
+})
